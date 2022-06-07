846bd925a99020a980f04b655ad96b4aef9757c4
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-06-07 09:28:26 -0500

    updaing repo

--- js/movies.js
+++ js/movies.js
@@ -4,3 +4,22 @@ const getAllMovies = () => {
 }
 
 console.log(getAllMovies());
+
+const renderMovieHTML = () => {
+    console.log("Rendering Movie HTML")
+    getAllMovies().then((data) => {
+        let movieCards = data.map(movie => {
+            return `
+            <div>
+            <h3>Title: ${movie.title}</h3>
+            <p>Rating: ${movie.rating}</p>
+            <button data-id="${movie.id}">Edit</button>
+            <button data-id="${movie.id}">Delete</button>
+            </div>
+            `
+        })
+        console.log(movieCards);
+        document.getElementById("library").innerHTML = movieCards.join("");
+    })
+}
+renderMovieHTML();
